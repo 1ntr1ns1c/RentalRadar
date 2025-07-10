8258f73076618bc31bd00a8abf85298fa6e2f102
1ntr1ns1c <ikipkogey@kabarak.ac.ke>
2025-07-10 16:40:19 +0300

    feat: add routes for Properties and Contact pages in the main application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import Login from './pages/Login.tsx';
 import Register from  './pages/Register.tsx'
 import Dashboard from './pages/Dashboard';
 import Navbar from './components/Navbar';
+import PropertiesPage from './pages/Properties.tsx';
+import Contact from './pages/Contact.tsx';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/properties" element={<PropertiesPage />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </AuthProvider>
