ff09b0645ef73712968450ccb17388f5ba38843b
1ntr1ns1c <ikipkogey@kabarak.ac.ke>
2025-07-10 16:37:57 +0300

    feat: set up Express server with initial routes and Sequelize integration

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const sequelize = require('./config/db');
+const authRoutes = require('./routes/auth');
+const propertyRoutes = require('./routes/properties');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/properties', propertyRoutes);
+
+// TODO: Register routes here
+
+// Test DB connection
+sequelize.authenticate()
+  .then(() => console.log('Database connected'))
+  .catch(err => console.error('DB connection error:', err));
+
+module.exports = app;
